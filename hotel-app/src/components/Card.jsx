af524997bec87c63b9051f07910b7c8f954ff579
Preeti Yadav <pyadav491@gmail.com>
2025-04-27 15:18:49 +1000

    add rating

--- hotel-app/src/components/Card.jsx
+++ hotel-app/src/components/Card.jsx
@@ -7,14 +7,35 @@ const Card = ({
     roomType, 
     price
 }) => {
+    const ratingDiamonds = (rating) => {
+        const diamonds = [];
+        const fullDiamonds = Math.floor(rating);
+        const hasHalfdiamonds = rating % 1 !== 0;
+
+        for(let i=1; i<=fullDiamonds; i++) {
+            diamonds.push(<span key={i} className="c-card__diamonds"></span>)
+        }
+
+        if(hasHalfdiamonds) {
+            diamonds.push(<span key="6" className="c-card__diamonds c-card__diamonds--half"></span>)
+        }
+
+        return diamonds;
+    };
+
     return (
         <div className="c-card">
             <div className="c-card__image" style={{backgroundImage:`url(${image})`}} />
             <div className="c-card__detail">
                 <div className="c-card__info">
                     <h5>{name}</h5>
-                    <span></span>
-                    <span>{roomType}</span>
+                    <span className="c-card__rating">
+                        {ratingDiamonds(rating)}
+                    </span>
+                    <span className="c-card__text">
+                        <span className="c-card__text-type">Room type:</span> 
+                        {roomType}
+                    </span>
                 </div>
                 <div className="c-card__price">
                     <span>{price}</span>
